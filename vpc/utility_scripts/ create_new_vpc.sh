70b95d288906b6e8bc9e0be74eebf147bb0158a0
anhtv08 <anhtv010888@gmail.com>
2019-05-13 07:33:44 +0800

    adding scripts to provision vpc

--- vpc/utility_scripts/ create_new_vpc.sh
+++ vpc/utility_scripts/ create_new_vpc.sh
@@ -5,4 +5,9 @@ source $aws_home_dir/setup_env.sh
 
 # create vpc using cloud formation.
 
+deploy_stack_cmd=$(aws cloudformation deploy --file-value=$aws_home_dir/vpc/templates/vpc_single_instance_into_subnet.template)
 
+exec $deploy_stack_cmd
+
+
+ 
