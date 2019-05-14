e9c5c13258b066748a265d19136009c1dac5172a
anhtv08 <anhtv010888@gmail.com>
2019-05-15 06:11:03 +0800

    correct stack name adding keypair name

--- vpc/utility_scripts/create_new_vpc.sh
+++ vpc/utility_scripts/create_new_vpc.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # load environment setup scripts
+export aws_home_dir=$HOME/working/cloud-computing/aws
 source $aws_home_dir/setup_env.sh
 
 # create vpc using cloud formation.
-vpc_stack_name="vpc_with_single_ec2_instance"
+# [a-zA-Z][-a-zA-Z]
+vpc_stack_name="vpc-with-single-ec2-instance"
+
 
 #validate template
 validate_stack_cmd="aws cloudformation validate-template --template-body $aws_home_dir/vpc/templates/vpc_single_instance_into_subnet.template"
 exec $validate_stack_cmd
 
-#deploy_stack_cmd="aws cloudformation deploy --stack-name $vpc_stack_name --template-file $aws_home_dir/vpc/templates/vpc_single_instance_into_subnet.template"
-#exec $validate_stack_cmd
+deploy_stack_cmd="aws cloudformation deploy --stack-name $vpc_stack_name --template-file $aws_home_dir/vpc/templates/vpc_single_instance_into_subnet.template"
+exec $validate_stack_cmd
 # ret_code=$?
 # if [[ $ret_code -ne 0 ]]; then
 #     echo "Validation is completed"
