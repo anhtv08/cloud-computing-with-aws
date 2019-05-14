728b3b8c3f485bd9ce3139d7118aeb94a2c2d839
anhtv08 <anhtv010888@gmail.com>
2019-05-15 06:38:30 +0800

    fixing creating stack

--- vpc/utility_scripts/create_new_vpc.sh
+++ vpc/utility_scripts/create_new_vpc.sh
@@ -7,12 +7,6 @@ source $aws_home_dir/setup_env.sh
 # create vpc using cloud formation.
 # [a-zA-Z][-a-zA-Z]
 vpc_stack_name="vpc-with-single-ec2-instance"
-
-
-#validate template
-validate_stack_cmd="aws cloudformation validate-template --template-body $aws_home_dir/vpc/templates/vpc_single_instance_into_subnet.template"
-exec $validate_stack_cmd
-
 deploy_stack_cmd="aws cloudformation deploy --stack-name $vpc_stack_name --template-file $aws_home_dir/vpc/templates/vpc_single_instance_into_subnet.template"
 exec $deploy_stack_cmd
 # ret_code=$?
