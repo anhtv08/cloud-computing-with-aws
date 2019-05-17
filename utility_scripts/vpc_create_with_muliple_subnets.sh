a58d93e2497431e5844237c188e4fd616ad4ff8c
anhtv08 <anhtv010888@gmail.com>
2019-05-18 05:53:01 +0800

    using  one nat instance for route traffic from private subnet to internet  to save money

--- utility_scripts/vpc_create_with_muliple_subnets.sh
+++ utility_scripts/vpc_create_with_muliple_subnets.sh
@@ -6,7 +6,7 @@ source $aws_home_dir/setup_env.sh
 
 # create vpc using cloud formation.
 # [a-zA-Z][-a-zA-Z]
-vpc_stack_name="vpc-with-multiple-public-private-subnets"
+vpc_stack_name="vpc-with-multiple-public-private-subnets-1"
 deploy_stack_cmd="aws cloudformation deploy --stack-name $vpc_stack_name --template-file $aws_home_dir/services/vpc/vpc_with_managed_NAT_And_Private_Subnet.yaml"
 exec $deploy_stack_cmd
 # ret_code=$?
--- utility_scripts/vpc_delete_with_muliple_subnets.sh
+++ utility_scripts/vpc_delete_with_muliple_subnets.sh
@@ -6,7 +6,7 @@ source $aws_home_dir/setup_env.sh
 
 # create vpc using cloud formation.
 # [a-zA-Z][-a-zA-Z]
-vpc_stack_name="vpc-with-multiple-public-private-subnets"
+vpc_stack_name="vpc-with-multiple-public-private-subnets-1"
 #validate template
 delete_stack_cmd="aws cloudformation delete-stack --stack-name $vpc_stack_name"
 exec $delete_stack_cmd
